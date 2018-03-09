259e4b3afc13c64d643551e71bcc7151f7209958
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-09 00:52:45 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,8 +1,9 @@
 
 def run_guessing_game
 puts "Guess a number between 1 and 6."
-  response = gets.chomp
-  random_number = 1+rand(6)
+response = gets.chomp
+random_number = 1+rand(6)
+
 if response == "exit"
   puts "Goodbye!"
   break
