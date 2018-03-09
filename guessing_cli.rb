5ea740972d70518619e956757bbeb83dc45a1ee9
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-09 00:43:45 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,8 +7,9 @@ random_number = 1+rand(6)
 if response == "exit"
   puts "Goodbye"
   break
-elsif response.to_i == random_number 
-  puts "You guessed the correct number!" 
+elsif response.to_i == random_number
+  puts "You guessed the correct number!"
 elsif response.to_i != randsom_number
   puts "The computer guessed #{random_number}."
+end
 end
