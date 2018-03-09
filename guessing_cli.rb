5ddd23258a5b9fdbd3a18b2ad98c178344e296f0
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-09 01:04:10 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -19,7 +19,7 @@ def run_guessing_game
 	user_input = gets.chomp.to_s
     if user_input == random_number
     	puts "You guessed the correct number!"
-    elsif user_input != roll
+    elsif user_input != random_number
     	puts "The computer guessed #{random_number}."
     elsif user_input == "exit"
       puts "Goodbye!"
