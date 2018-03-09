246d8356fdab6be008d1892a528d93c8f882cb25
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-09 00:49:21 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,14 +4,15 @@ puts "Guess a number between 1 and 6."
 response = gets.chomp
 random_number = 1+rand(6)
 
-while response
+
 if response == "exit"
   puts "Goodbye!"
   break
+  while response != "exit"
 elsif response.to_i == random_number
   puts "You guessed the correct number!"
 elsif response.to_i != random_number
   puts "The computer guessed #{random_number}."
-end
-end
+    end
+  end
 end
